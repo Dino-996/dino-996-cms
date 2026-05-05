aea162c2919d3c66407e8bab8df55836d3ec69ab
dino-996 <davidesabia22@gmail.com>
2026-05-05 04:16:29 +0200

    fix: update body middleware configuration for improved file handling

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -17,11 +17,20 @@ const config: Core.Config.Middlewares = [
       },
     },
   },
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '256mb',
+      jsonLimit: '256mb',
+      textLimit: '256mb',
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024, // 250mb
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
